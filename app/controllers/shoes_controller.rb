b6fa03b894f12bf36aa578061e7556f2491e97c3
Shea Kusiak <shkusiak@gmail.com>
2018-08-31 01:42:28 +0000

    added if not logged_in? logic to shoe_controller get requests

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,19 +1,35 @@
 class ShoesController < ApplicationController
 
   get '/shoes' do
-
+    if !logged_in?
+      redirect to '/login'
+    else
+      erb :'/shoes/shoes'
+    end
   end
 
   get '/shoes/new' do
-
+    if !logged_in?
+      redirect to '/login'
+    else
+      erb :'shoes/create_shoe'
+    end
   end
 
   get '/shoes/:id' do
-
+    if !logged_in?
+      redirect to '/login'
+    else
+      erb :'/shoes/show_shoe'
+    end
   end
 
   get '/shoes/:id/edit' do
-
+    if !logged_in?
+      redirect to '/login'
+    else
+      erb :'/shoes/edit_shoe'
+    end
   end
 
   post '/shoes' do
