96a9fcb537e57479ffeb1d10d2c8d8499025c23a
Shea Kusiak <shkusiak@gmail.com>
2018-09-19 02:00:59 +0000

    developed 'post' for '/signup'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,16 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+      redirect to '/signup'
+    elsif user_exists?(params)
+      redirect to '/login'
+    else
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/shoes'
+    end
   end
 
   get '/login' do
