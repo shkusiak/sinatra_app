cad64d0474754939bde382bad0dbd3066ba0457f
Shea Kusiak <shkusiak@gmail.com>
2018-09-22 19:31:59 +0000

    fixed migration for CreateUsers:

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 20180831001347) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
