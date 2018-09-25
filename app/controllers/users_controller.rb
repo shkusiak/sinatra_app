ac9bfb951809a7de070d98c0e6a43a2301e92087
Shea Kusiak <shkusiak@gmail.com>
2018-09-25 00:18:43 +0000

    added #slug and .find_by_slug(slug) methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+  
   get '/signup' do
     if session[:user_id]
       redirect to '/shoes'
